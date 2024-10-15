1ff1534cf962cb7ff60b7cdd7d563f0b65ec5a55
eisenhowair <elias.mouaheb@gmail.com>
2024-10-15 09:57:02 +0200

    corrected setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+sudo chown -R $(whoami):$(whoami) ~/OrientationLLM/ProjetLLM # pour avoir les droits sur l'environnement
 export TMPDIR='/var/tmp'
-pip install -r requirements.txt
+pip3 install --no-cache-dir -r requirements.txt
+chainlit create-secret
+
+echo "Si problème d'espace sur le périphérique, relancer ce fichier"
+# si problème de place: rm -rf ~/.cache/pip
+# si problème d'installation dans le venv, supprimer le venv et le recréer manuellement
