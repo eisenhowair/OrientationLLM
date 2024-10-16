e478f754b6cf5fb7272af41bf3ff16be79d8cc4f
eisenhowair <elias.mouaheb@gmail.com>
2024-10-16 16:09:32 +0200

    update pip

--- setup_env.sh
+++ setup_env.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo chown -R $(whoami):$(whoami) ~/OrientationLLM/ProjetLLM # pour avoir les droits sur l'environnement
 export TMPDIR='/var/tmp'
+pip3 install --upgrade wheel setuptools
 pip3 install --no-cache-dir -r requirements.txt
 
 echo "Si problème d'espace sur le périphérique, relancer ce fichier"
